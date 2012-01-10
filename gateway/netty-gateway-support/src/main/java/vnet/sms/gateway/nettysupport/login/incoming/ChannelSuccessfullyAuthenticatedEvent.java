501abce4a21a71d61a57d8f06bc14717572f2944
Olaf Bergner <olaf.bergner@gmx.de>
2012-01-10 23:52:58 +0100

    14: GatewayServer: start to send outgoing pings only after a channel has been authenticated  * Extract abstract base class for ChannelEvents having a UUID and a creation timestamp. Status: Incomplete

--- gateway/netty-gateway-support/src/main/java/vnet/sms/gateway/nettysupport/login/incoming/ChannelSuccessfullyAuthenticatedEvent.java
+++ gateway/netty-gateway-support/src/main/java/vnet/sms/gateway/nettysupport/login/incoming/ChannelSuccessfullyAuthenticatedEvent.java
@@ -5,29 +5,17 @@ package vnet.sms.gateway.nettysupport.login.incoming;
 
 import static org.apache.commons.lang.Validate.notNull;
 
-import java.util.Date;
-import java.util.UUID;
-
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelEvent;
-import org.jboss.netty.channel.ChannelFuture;
-import org.jboss.netty.channel.Channels;
 
 import vnet.sms.common.messages.LoginRequest;
+import vnet.sms.gateway.nettysupport.IdentifiableChannelEvent;
 
 /**
  * @author obergner
  * 
  */
-public class ChannelSuccessfullyAuthenticatedEvent implements ChannelEvent {
-
-	private final UUID	        id;
-
-	private final long	        creationTimestamp;
-
-	private final Channel	    channel;
-
-	private final ChannelFuture	future;
+public class ChannelSuccessfullyAuthenticatedEvent extends
+        IdentifiableChannelEvent {
 
 	private final LoginRequest	successfulLoginRequest;
 
@@ -37,32 +25,10 @@ public class ChannelSuccessfullyAuthenticatedEvent implements ChannelEvent {
 	 */
 	ChannelSuccessfullyAuthenticatedEvent(final Channel channel,
 	        final LoginRequest successfulLoginRequest) {
-		notNull(channel, "Argument 'channel' must not be null");
+		super(channel);
 		notNull(successfulLoginRequest,
 		        "Argument 'successfulLoginRequest' must not be null");
-		this.id = UUID.randomUUID();
-		this.creationTimestamp = System.currentTimeMillis();
-		this.channel = channel;
-		this.future = Channels.succeededFuture(channel);
 		this.successfulLoginRequest = successfulLoginRequest;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public final UUID getId() {
-		return this.id;
-	}
-
-	/**
-	 * @return the creationTimestamp
-	 */
-	public final long getCreationTimestamp() {
-		return this.creationTimestamp;
-	}
-
-	public final Date getCreationTime() {
-		return new Date(this.creationTimestamp);
 	}
 
 	/**
@@ -72,56 +38,11 @@ public class ChannelSuccessfullyAuthenticatedEvent implements ChannelEvent {
 		return this.successfulLoginRequest;
 	}
 
-	/**
-	 * @see org.jboss.netty.channel.ChannelEvent#getChannel()
-	 */
-	@Override
-	public Channel getChannel() {
-		return this.channel;
-	}
-
-	/**
-	 * @see org.jboss.netty.channel.ChannelEvent#getFuture()
-	 */
-	@Override
-	public ChannelFuture getFuture() {
-		return this.future;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final ChannelSuccessfullyAuthenticatedEvent other = (ChannelSuccessfullyAuthenticatedEvent) obj;
-		if (this.id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!this.id.equals(other.id)) {
-			return false;
-		}
-		return true;
-	}
-
 	@Override
 	public String toString() {
 		return "ChannelSuccessfullyAuthenticatedEvent@" + this.hashCode()
-		        + "[id: " + this.id + "|channel: " + this.channel
+		        + "[id: " + getId() + "|creationTimestamp: "
+		        + getCreationTimestamp() + "|channel: " + getChannel()
 		        + "|successfulLoginRequest: " + this.successfulLoginRequest
 		        + "]";
 	}
--- gateway/netty-gateway-support/src/main/java/vnet/sms/gateway/nettysupport/ping/outgoing/NoPingResponseReceivedWithinTimeoutEvent.java
+++ gateway/netty-gateway-support/src/main/java/vnet/sms/gateway/nettysupport/ping/outgoing/NoPingResponseReceivedWithinTimeoutEvent.java
@@ -3,44 +3,26 @@
  */
 package vnet.sms.gateway.nettysupport.ping.outgoing;
 
-import java.util.Date;
-import java.util.UUID;
-
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelEvent;
-import org.jboss.netty.channel.ChannelFuture;
-import org.jboss.netty.channel.Channels;
+
+import vnet.sms.gateway.nettysupport.IdentifiableChannelEvent;
 
 /**
  * @author obergner
  * 
  */
-public final class NoPingResponseReceivedWithinTimeoutEvent implements
-        ChannelEvent {
+public final class NoPingResponseReceivedWithinTimeoutEvent extends
+        IdentifiableChannelEvent {
 
-	private final UUID	        id	              = UUID.randomUUID();
+	private final int	pingIntervalSeconds;
 
-	private final Channel	    channel;
-
-	private final ChannelFuture	future;
-
-	private final int	        pingIntervalSeconds;
-
-	private final long	        pingResponseTimeoutMillis;
-
-	private final long	        creationTimestamp	= System
-	                                                      .currentTimeMillis();
+	private final long	pingResponseTimeoutMillis;
 
 	public NoPingResponseReceivedWithinTimeoutEvent(final Channel channel,
 	        final int pingIntervalSeconds, final long pingResponseTimeoutMillis) {
-		this.channel = channel;
-		this.future = Channels.succeededFuture(channel);
+		super(channel);
 		this.pingIntervalSeconds = pingIntervalSeconds;
 		this.pingResponseTimeoutMillis = pingResponseTimeoutMillis;
-	}
-
-	public UUID getId() {
-		return this.id;
 	}
 
 	public int getPingIntervalSeconds() {
@@ -51,61 +33,13 @@ public final class NoPingResponseReceivedWithinTimeoutEvent implements
 		return this.pingResponseTimeoutMillis;
 	}
 
-	public long getCreationTimestamp() {
-		return this.creationTimestamp;
-	}
-
-	public Date getCreationDate() {
-		return new Date(this.creationTimestamp);
-	}
-
-	@Override
-	public Channel getChannel() {
-		return this.channel;
-	}
-
-	@Override
-	public ChannelFuture getFuture() {
-		return this.future;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final NoPingResponseReceivedWithinTimeoutEvent other = (NoPingResponseReceivedWithinTimeoutEvent) obj;
-		if (this.id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!this.id.equals(other.id)) {
-			return false;
-		}
-		return true;
-	}
-
 	@Override
 	public String toString() {
 		return "NoPingResponseReceivedWithinTimeoutEvent@" + this.hashCode()
-		        + " [id: " + this.id + "|channel: " + this.channel
-		        + "|future: " + this.future + "|pingIntervalSeconds: "
-		        + this.pingIntervalSeconds + "|pingResponseTimeoutMillis: "
+		        + " [id: " + getId() + "|channel: " + getChannel()
+		        + "|pingIntervalSeconds: " + this.pingIntervalSeconds
+		        + "|pingResponseTimeoutMillis: "
 		        + this.pingResponseTimeoutMillis + "|creationTimestamp: "
-		        + this.creationTimestamp + "]";
+		        + getCreationTimestamp() + "]";
 	}
 }
