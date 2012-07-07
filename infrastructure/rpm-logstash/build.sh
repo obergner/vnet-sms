a2fc7453e8a312e69ee02f20c0eda56b12dd587d
Olaf Bergner <olaf.bergner@gmx.de>
2012-07-07 21:29:15 +0200

    52: Infrastructure: create a logstash rpm  * First cut at logstash rpm Status: Incomplete

---
+++ infrastructure/rpm-logstash/build.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+set -e
+set -o nounset
+
+VERSION="$1"
+RELEASE="$2"
+DIST="$3"
+ARCH="$4"
+REPO="$5"
+
+MODULEDIR=$( cd "$( dirname "$0" )" && pwd )
+RPMBUILD=${HOME}/rpmbuild
+PACKAGE=logstash-${VERSION}-${RELEASE}.${DIST}.${ARCH}.rpm
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Building RPM for logstash ${VERSION} ..."
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Cleaning rpmbuild directory in ${RPMBUILD} (PRE) ..."
+rm -rf ${RPMBUILD}
+echo "rpmbuild directory ${RPMBUILD} cleaned (PRE)"
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Creating fresh rpmbuild directory in ${RPMBUILD} ..."
+/usr/bin/rpmdev-setuptree
+echo "Fresh rpmbuild directory in ${RPMBUILD} created"
+echo "------------------------------------------------------------------------"
+
+pushd ${RPMBUILD}
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Symlinking sources from ${MODULEDIR} into ${RPMBUILD} ..."
+ln -s ${MODULEDIR}/target/rpm/SPECS/logstash.spec ${RPMBUILD}/SPECS/logstash.spec
+ln -s ${MODULEDIR}/target/rpm/SOURCES/* ${RPMBUILD}/SOURCES/
+echo "Symlinked sources from ${MODULEDIR} into ${RPMBUILD} ..."
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Downloading logstash sources ..."
+/usr/bin/spectool -g ${RPMBUILD}/SPECS/logstash.spec
+echo "Finished downloading logstash sources"
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Moving downloaded archive to ${RPMBUILD}/SOURCES ..."
+mv ${RPMBUILD}/logstash-${VERSION}.tar.gz ${RPMBUILD}/SOURCES
+echo "Moved downloaded archive to ${RPMBUILD}/SOURCES"
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Building logstash binary rpm ..."
+/usr/bin/rpmbuild --define "dist .${DIST}" -bb ${RPMBUILD}/SPECS/logstash.spec
+echo "Finished building logstash binary rpm ..."
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Copying logstash binary rpm to ${MODULEDIR}/target ..."
+mkdir -p ${MODULEDIR}/target
+cp -R ${RPMBUILD}/RPMS/* ${MODULEDIR}/target/
+echo "Copied logstash binary rpm to ${MODULEDIR}/target ..."
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Uploading rpm to pulp server ..."
+/usr/bin/pulp-admin -u admin -p admin content upload --nosig --verbose --dir ${MODULEDIR}/target/x86_64/
+echo "Uploaded rpm to pulp server"
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Associating generated rpm ${PACKAGE} with pulp repository ${REPO} ..."
+/usr/bin/pulp-admin -u admin -p admin repo add_package --id ${REPO} --package=${PACKAGE}
+echo "Associated generated rpm ${PACKAGE} with pulp repository ${REPO}"
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Scheduling metadata update for pulp repository ${REPO} ..."
+/usr/bin/pulp-admin -u admin -p admin repo generate_metadata --id ${REPO}
+echo "Scheduled metadata update for pulp repository ${REPO}"
+echo "------------------------------------------------------------------------"
+
+popd
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Cleaning rpmbuild directory in ${RPMBUILD} (POST)..."
+rm -rf ${RPMBUILD}
+echo "rpmbuild directory ${RPMBUILD} cleaned (POST)"
+echo "------------------------------------------------------------------------"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Finished building RPM for logstash ${VERSION}"
+echo "------------------------------------------------------------------------"
+echo ""
---
+++ infrastructure/rpm-logstash/src/main/rpm/SPECS/logstash.spec
@@ -0,0 +1,55 @@
+%define debug_package %{nil}
+
+Name:           logstash
+Version:        ${logstash.version}
+Release:        ${buildNumber}%{?dist}
+Summary:        logstash is a tool for managing events and logs.
+
+Group:          System Environment/Daemons
+License:        Apache 2.0
+URL:            http://logstash.net
+Source0:        http://semicomplete.com/files/logstash/%{name}-%{version}-monolithic.jar
+Source1:        etc-rc.d-init.d-logstash
+Source2:        etc-logstash-logstash.conf
+Source3:        etc-logstash-log4j.properties
+Source4:        usr-sbin-logstash
+
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+BuildArch:      noarch
+
+Requires:       jdk
+Requires:       grok
+# Requires:       java
+
+Requires:       chkconfig initscripts
+
+# disable jar repackaging
+%define __os_install_post %{nil}
+
+%description
+logstash is a tool for managing events and logs. You can use it to collect logs, parse them, and store them for later use (like, for searching).
+
+%prep
+cp -p %SOURCE0 %SOURCE1 %SOURCE2 %SOURCE3 %SOURCE4 .
+find . -type f -print0 | xargs -0 --no-run-if-empty -- sed -i -e 's/@@@version@@@/%{version}/g'
+
+%install
+rm -rf "${RPM_BUILD_ROOT}"
+mkdir -p "${RPM_BUILD_ROOT}/usr/share/logstash/"
+install -D -m 644 -t "${RPM_BUILD_ROOT}/usr/share/logstash/" *.jar
+install -D -m 755 etc-rc.d-init.d-logstash          "${RPM_BUILD_ROOT}/etc/rc.d/init.d/logstash"
+install -D -m 644 etc-logstash-logstash.conf        "${RPM_BUILD_ROOT}/etc/logstash/logstash.conf"
+install -D -m 644 etc-logstash-log4j.properties     "${RPM_BUILD_ROOT}/etc/logstash/log4j.properties"
+install -D -m 755 usr-sbin-logstash                 "${RPM_BUILD_ROOT}/usr/sbin/logstash"
+mkdir -p "${RPM_BUILD_ROOT}/var/lib/logstash"
+
+%clean
+rm -rf %{buildroot}
+
+%files
+%defattr(-,root,root,-)
+/usr/
+/etc/
+/var/
+
+%changelog
