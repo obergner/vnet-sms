ad06b6794cfa0ac9652ee03a58aafdea7bd0edc8
Olaf Bergner <olaf.bergner@gmx.de>
2012-07-07 18:55:15 +0200

    50: Infrastructure: Create an elasticsearch rpm  * Make build script a little more robust Status: Incomplete

--- infrastructure/rpm-elasticsearch/build.sh
+++ infrastructure/rpm-elasticsearch/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+set -o nounset
+
 VERSION="$1"
 MODULEDIR=$( cd "$( dirname "$0" )" && pwd )
 RPMBUILD=${HOME}/rpmbuild
@@ -11,22 +14,22 @@ rm -rf ${RPMBUILD}
 echo "rpmbuild directory ${RPMBUILD} cleaned"
 
 echo "Creating fresh rpmbuild directory in ${RPMBUILD} ..."
-/usr/bin/rpmdev-setuptree || exit 1
+/usr/bin/rpmdev-setuptree
 echo "Fresh rpmbuild directory in ${RPMBUILD} created"
 
 pushd ${RPMBUILD}
 
 echo "Symlinking sources from ${MODULEDIR} into ${RPMBUILD} ..."
-ln -s ${MODULEDIR}/target/rpm/SPECS/elasticsearch.spec ${RPMBUILD}/SPECS/elasticsearch.spec || exit 1
-ln -s ${MODULEDIR}/target/rpm/SOURCES/* ${RPMBUILD}/SOURCES/ || exit 1
+ln -s ${MODULEDIR}/target/rpm/SPECS/elasticsearch.spec ${RPMBUILD}/SPECS/elasticsearch.spec
+ln -s ${MODULEDIR}/target/rpm/SOURCES/* ${RPMBUILD}/SOURCES/
 echo "Symlinked sources from ${MODULEDIR} into ${RPMBUILD} ..."
 
 echo "Downloading elasticsearch sources ..."
-/usr/bin/spectool -g ${RPMBUILD}/SPECS/elasticsearch.spec  || exit 1
+/usr/bin/spectool -g ${RPMBUILD}/SPECS/elasticsearch.spec
 echo "Finished downloading elasticsearch sources"
 
 echo "Moving downloaded archive to ${RPMBUILD}/SOURCES ..."
-mv ${RPMBUILD}/elasticsearch-${VERSION}.tar.gz ${RPMBUILD}/SOURCES || exit 1
+mv ${RPMBUILD}/elasticsearch-${VERSION}.tar.gz ${RPMBUILD}/SOURCES
 echo "Moved downloaded archive to ${RPMBUILD}/SOURCES"
 
 # echo "Building elasticsearch source rpm ..."
@@ -34,17 +37,16 @@ echo "Moved downloaded archive to ${RPMBUILD}/SOURCES"
 # echo "Finished building elasticsearch source rpm"
 
 echo "Building elasticsearch binary rpm ..."
-/usr/bin/rpmbuild -bb ${RPMBUILD}/SPECS/elasticsearch.spec || exit 1
+/usr/bin/rpmbuild -bb ${RPMBUILD}/SPECS/elasticsearch.spec
 echo "Finished building elasticsearch binary rpm ..."
 
 echo "Copying elasticsearch binary rpm to ${MODULEDIR}/target ..."
 mkdir -p ${MODULEDIR}/target
-#rm -rf ${MODULEDIR}/target/*
 cp -R ${RPMBUILD}/RPMS/* ${MODULEDIR}/target/
 echo "Copied elasticsearch binary rpm to ${MODULEDIR}/target ..."
 
 echo "Uploading rpm to pulp repository ..."
-/usr/bin/pulp-admin -u admin -p admin content upload --nosig --verbose --dir ${MODULEDIR}/target/x86_64/ || exit 1
+/usr/bin/pulp-admin -u admin -p admin content upload --nosig --verbose --dir ${MODULEDIR}/target/x86_64/
 echo "Uploaded rpm to pulp repository"
 
 popd
